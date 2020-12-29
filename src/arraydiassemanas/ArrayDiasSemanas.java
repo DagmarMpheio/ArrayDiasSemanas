34548ff49c3381aaba06a5313c0a44408d50b364
DagmarMpheio <dagmarmpheiu@gmail.com>
2020-12-29 19:27:24 +0100

    Primeiro Commit

---
+++ src/arraydiassemanas/ArrayDiasSemanas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arraydiassemanas;
+
+/**
+ *
+ * @author dagma
+ */
+public class ArrayDiasSemanas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String semanas[]={"Segunda", "Terça","Quarta", "Quinta", "Sexta", "Sabado", "Domingo"};
+        int i=0;
+        while (i<semanas.length){
+            System.out.println(semanas[i]);
+            i++;
+        }
+    }
+    
+}
